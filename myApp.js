bd36c1e5f944752a796b213288e80dcb9f5329c7
AfraIsNotAvailable <afrasinei.serban@gmail.com>
2023-10-14 15:22:40 +0300

    implemented a simple rest api

--- myApp.js
+++ myApp.js
@@ -9,6 +9,10 @@ app.get("/", function(req, res) {
     res.sendFile(absolutePath);
 });
 
+app.get("/json", function (req, res) {
+    res.json({"message": "Hello json"});
+});
+
 app.use("/public", express.static(__dirname + "/public"));
 
 
