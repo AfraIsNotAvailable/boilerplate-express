e017231f12907226a29aff06e7bbb00670fbac83
AfraIsNotAvailable <afrasinei.serban@gmail.com>
2023-10-14 15:34:58 +0300

    uppercase json

--- myApp.js
+++ myApp.js
@@ -1,5 +1,6 @@
 let express = require('express');
 let app = express();
+require('dotenv').config();
 
 console.log('Hello World');
 
@@ -10,7 +11,11 @@ app.get("/", function(req, res) {
 });
 
 app.get("/json", function (req, res) {
-    res.json({"message": "Hello json"});
+    if (process.env.MESSAGE_STYLE === "uppercase") {
+        res.json({"message": "HELLO JSON"});
+    } else {
+        res.json({"message": "Hello json"});
+    }
 });
 
 app.use("/public", express.static(__dirname + "/public"));
