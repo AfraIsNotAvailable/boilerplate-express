1e7748261fabbaf1c3e0388872336b292442d293
AfraIsNotAvailable <afrasinei.serban@gmail.com>
2023-10-15 13:25:39 +0300

    asa

--- myApp.js
+++ myApp.js
@@ -5,13 +5,12 @@ require('dotenv').config()
 console.log('Hello World');
 
 app.get("/", function(req, res) {
-    // res.send("Hello Express");
     absolutePath = __dirname + "/views/index.html";
     res.sendFile(absolutePath);
 });
 
 app.get("/json", function (req, res) {
-    if (process.env.MESSAGE_STYLE === "uppercase") {
+    if (process.env.MESSAGE_STYLE == "uppercase") {
         res.json({"message": "HELLO JSON"});
     } else {
         res.json({"message": "Hello json"});
