23c3cabb705bc10ccede694ac57fb11c1c14a5dd
AfraIsNotAvailable <afrasinei.serban@gmail.com>
2023-10-18 00:18:37 +0300

    added something idk anymore

--- myApp.js
+++ myApp.js
@@ -1,72 +1,48 @@
-let express = require('express');
+let express = require("express");
+const res = require("express/lib/response");
 let app = express();
-require('dotenv').config()
+require("dotenv").config();
 
-console.log('Hello World');
+console.log("Hello World");
 
 app.use(function (req, res, next) {
     console.log(req.method + " " + req.path + " - " + req.ip);
     next();
 });
 
-app.get("/", function(req, res) {
+app.get("/", function (req, res) {
     absolutePath = __dirname + "/views/index.html";
     res.sendFile(absolutePath);
 });
 
-app.get("/now", function (req, res, next) {
-    req.time = new Date().toString();
-    next();
-}, function (req, res) {
-    res.send({ time: req.time });
+app.get("/:word/echo", (req, res) => {
+    res.json({ echo: req.params.word });
 });
+
+app.get(
+    "/now",
+    function (req, res, next) {
+        req.time = new Date().toString();
+        next();
+    },
+    function (req, res) {
+        res.send({ time: req.time });
+    }
+);
 
 app.get("/json", function (req, res) {
     response = "Hello json";
 
     if (process.env.MESSAGE_STYLE === "uppercase") {
         response = response.toUpperCase();
-    }else{
+    } else {
         response = "Hello json";
     }
 
-    res.json({ "message": response });
+    res.json({ message: response });
     //does it work?
 });
 
 app.use("/public", express.static(__dirname + "/public"));
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+module.exports = app;
